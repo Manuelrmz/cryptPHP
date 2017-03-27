081ae98af7efdab6e4282e1a238455aedd5c52fc
Manuel Ramirez <manuel.ramirez89@gmail.com>
2017-03-27 17:51:27 -0600

    Documentado los metodos y prueba en el index

--- crypt.php
+++ crypt.php
@@ -7,15 +7,20 @@ class crypt
 	{
 		$this->setKey($key);
 	}
-	private function Decrypt($parameter)
+	/**
+	 * [Crypt]
+	 * @param [string] $text
+	 * 
+	 * @return  [string] [Encrypted Text]
+	 */
+	private function Crypt($text)
 	{
-		$C = 0;
 		$T = 0;
 		$i = 0;
 		$j = 0;
 		$cadena = "";
 		$this->createKeyArray();
-		for($C; $C < strlen($parameter); $C++)
+		for($C = 0; $C < strlen($text); $C++)
 		{
 			$i = ($i + 1) & 255;
 			$j = ($j + $this->_s[$i]) & 255;
@@ -23,10 +28,40 @@ class crypt
 			$this->_s[$i] = $this->_s[$j];
 			$this->_s[$j] = $T;
 			$T = ($this->_s[$i] + $this->_s[$j]) & 255;
-			$cadena = $cadena . chr(ord(substr($parameter,$C,1)) ^ $this->_s[$T]);
+			$cadena = $cadena . chr(ord(substr($text,$C,1)) ^ $this->_s[$T]);
 		}
 		return $cadena;
 	}
+	/**
+	 * [Decrypt]
+	 * @param [string] $text
+	 * 
+	 * @return  [string] [Decrypted Text]
+	 */
+	private function Decrypt($text)
+	{
+		$T = 0;
+		$i = 0;
+		$j = 0;
+		$cadena = "";
+		$this->createKeyArray();
+		for($C = 0; $C < strlen($text); $C++)
+		{
+			$i = ($i + 1) & 255;
+			$j = ($j + $this->_s[$i]) & 255;
+			$T = $this->_s[$i];
+			$this->_s[$i] = $this->_s[$j];
+			$this->_s[$j] = $T;
+			$T = ($this->_s[$i] + $this->_s[$j]) & 255;
+			$cadena = $cadena . chr(ord(substr($text,$C,1)) ^ $this->_s[$T]);
+		}
+		return $cadena;
+	}
+	/**
+	 * [createdKeyArray]
+	 * 
+	 * @return nothing
+	 */
 	private function createKeyArray()
 	{
 		$KeyLenght = 0;
@@ -43,7 +78,6 @@ class crypt
 			for($i = 0; $i < 256; $i++)
 			{
 				$j = ($j + $this->_s[$i] + ord(substr($this->getKey(),($i % $KeyLenght),1)) & 255);
-				//echo "J: ".$j." I: ".$i." , Lenght: ".$KeyLenght." , Mod: ".($i % $KeyLenght)."</br>";
 				$T = $this->_s[$i];
 				$this->_s[$i] = $this->_s[$j];
 				$this->_s[$j] = $T;
@@ -52,13 +86,19 @@ class crypt
 		else
 			throw new Exception("Key didn't found", 1);
 	}
-	private function ConvToInt($x)
+	/**
+	 * [ConvToInt]
+	 * @param [string] $hexValue
+	 *
+	 * @return [integer] $temp
+	 */
+	private function ConvToInt($hexValue)
 	{	
 		$x1 = "";
 		$x2 = "";
 		$temp = 0;
-		$x1 = substr($x,0,1);
-		$x2 = substr($x,1,1);
+		$x1 = substr($hexValue,0,1);
+		$x2 = substr($hexValue,1,1);
 		if(is_numeric ($x1))
 			$temp = 16 * $x1;
 		else
@@ -69,29 +109,110 @@ class crypt
 			$temp = $temp + (ord($x2) - 55);
 		return $temp;
 	}
-	private function desencriptar($dataValue)
+	/**
+	 * [ConvToHex]
+	 * @param [integer] $intvalue
+	 *
+	 * @return [string] $currentValue
+	 */
+	private function ConvToHex($intvalue)
+	{
+		$currentValue = "";
+		if($intvalue > 9)
+			$currentValue = chr($intvalue + 55);
+		else
+			$currentValue .= $intvalue;
+		return $currentValue;
+	}
+	/**
+	 * [encriptar]
+	 * @param [string] $text
+	 *
+	 * @return [string] $temp
+	 */
+	private function encriptar($text)
+	{
+		$x = 0;
+		$temp = "";
+		$tempNum = "";
+		$tempChar = "";
+		$tempChar2 = "";
+		for($x; $x < strlen($text); $x++)
+		{
+			$tempChar2 = substr($text,$x,1);
+			$tempNum = (integer)(ord($tempChar2) / 16);
+			if(($tempNum * 16) < ord($tempChar2))
+			{	
+				$tempChar = $this->ConvToHex(ord($tempChar2) - ($tempNum * 16));
+				$temp .= $this->ConvToHex($tempNum) . $tempChar;
+			}
+			else
+				$temp .= $this->ConvToHex($tempNum) . '0';
+		}
+		return $temp;
+	}
+	/**
+	 * [desencriptar]
+	 * @param [string] $text
+	 *
+	 * @return [string] $temp
+	 */
+	private function desencriptar($text)
 	{
 		$temp = "";
 		$HexByte = "";
-		for($i = 0; $i < strlen($dataValue); $i+=2)
+		for($i = 0; $i < strlen($text); $i+=2)
 		{
-			$HexByte = substr($dataValue,$i,2);
+			$HexByte = substr($text,$i,2);
 			$temp = $temp . chr($this->ConvToInt($HexByte));
 		}
 		return $temp;
 	}
+	/**
+	 * [DecryptString]
+	 * 
+	 * @param [string] $cadena
+	 * @param [string] $key
+	 *
+	 * @return [string] $decryptString
+	 */
 	public function DecryptString($cadena,$key)
 	{
 		$this->setKey($key);
 		$AuxDec = $this->desencriptar($cadena);
-		$decriptString = $this->Decrypt($AuxDec);
-		echo $decriptString;
+		$decryptString = $this->Decrypt($AuxDec);
+		return $decryptString;
 	}
-	/*Propertys*/
+	/**
+	 * [CryptString]
+	 * 
+	 * @param [string] $cadena
+	 * @param [string] $key
+	 *
+	 * @return [string] $encryptedString
+	 */
+	public function CryptString($cadena,$key)
+	{
+		$encryptedString = "";
+		$this->setKey($key);
+		if(trim($cadena) !== "")
+		{
+			$encryptedString  = $this->Crypt($cadena);
+		}
+		$encryptedString = $this->encriptar($encryptedString);
+		return $encryptedString;
+	}
+	/**
+	 * @return [string] $clsKey
+	 */
 	public function getKey()
 	{
 		return $this->clsKey;
 	}
+	/**
+	 * @param [string] $key
+	 * @return  [string] [Key Value as string]
+	 */
 	public function setKey($key)
 	{
 		$this->clsKey = (string)$key;
--- index.php
+++ index.php
@@ -2,8 +2,17 @@
 require_once "crypt.php";
 try
 {
-	$obj = new crypt("QYGgbSPf5gU9TY");
-	echo $obj->DecryptString("875B989D827F001F4AEA71","QYGgbSPf5gU9TY");
+	$key = "QYGgbSPf5gU9TY";
+	$originalString = "hola mundo";
+	$obj = new crypt($key);
+	echo 'Key: '.$key.'</br>';
+	echo 'Value: '.$originalString.'</br>';
+	$encrypted =  $obj->CryptString($originalString,"QYGgbSPf5gU9TY");
+	echo 'Encripted Value: '.$encrypted.'</br>';
+	echo 'Key: '.$key.'</br>';
+	echo 'Value: '.$encrypted.'</br>';
+	echo 'Decripted Value: ';
+	echo $obj->DecryptString($encrypted,$key).'</br>';
 }
 catch(Exception $ex)
 {
